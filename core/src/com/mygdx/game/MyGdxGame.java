082a20ce04c2643ef8957b65910ef7b5e1928c76
anamaksimova <19cherrytree91@gmail.com>
2022-08-19 15:59:43 +0500

    lesson1-1

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,25 +10,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	int clickes;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("head.jpg");
-	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(0, 0, 0, 1);
-		float x = Gdx.input.getX()- img.getWidth()/2;
-		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
-		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) clickes++;
-		Gdx.graphics.setTitle("Clicked" + clickes + "times!");
-		batch.begin();
-		batch.draw(img, x, y);
-		batch.end();
-	}
 	
 	@Override
 	public void dispose () {
